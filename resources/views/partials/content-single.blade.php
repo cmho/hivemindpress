d88a599d36ba515945b7ece52f75fe61478b4505
Carly Ho <cho@cliquestudios.com>
2020-01-11 14:03:14 -0600

    initial commit

---
+++ resources/views/partials/content-single.blade.php
@@ -0,0 +1,15 @@
+<article @php post_class() @endphp>
+  <header>
+    <h1 class="entry-title">{!! get_the_title() !!} - {{ __('By', 'sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
+    {{ get_the_author() }}
+  </a></h1>
+    @include('partials/entry-meta')
+  </header>
+  <div class="entry-content">
+    @php the_content() @endphp
+  </div>
+  <footer>
+    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
+  </footer>
+  @php comments_template('/partials/comments.blade.php') @endphp
+</article>
