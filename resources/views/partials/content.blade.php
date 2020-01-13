6e2b0fcf887f744254b33ed4661f8ebc00ce35a3
Carly Ho <cho@cliquestudios.com>
2020-01-13 16:20:12 -0600

    adjust templates

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,7 +1,6 @@
 <article @php post_class() @endphp>
   <header>
-    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a> - {{ __('By', 'sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
-    {{ get_the_author() }}
+    <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a>
   </a></h2>
     @include('partials/entry-meta')
   </header>
--- resources/views/taxonomy-work-author.blade.php
+++ resources/views/taxonomy-work-author.blade.php
@@ -9,22 +9,32 @@
     $twitter = pods_field('work-author', get_queried_object()->term_id, 'twitter', true);
     $insta = pods_field('work-author', get_queried_object()->term_id, 'instagram', true);
 @endphp
-  <h2>Work by {{ $author }}</h2>
+    <h2>Work by {{ $author }}</h2>
     <div class="bio">
         {!! $bio !!}
     </div>
     <ul class="social">
         @if($website && $website != "")
-            <li><a href="{{$website}}" rel="external" target="_blank"></a></li>
+            <li><a href="{{$website}}" rel="external" target="_blank"><i class="fas fa-link"></i><span class="sr-only">Website</span></a></li>
+        @endif
+        @if($itchio && $itchio != "")
+            <li><a href="{{$itchio}}" rel="external" target="_blank"><i class="fas fa-gamepad"></i><span class="sr-only">Itch.io</span></a></li>
+        @endif
+        @if($twitter && $twitter != "")
+            <li><a href="{{$twitter}}" rel="external" target="_blank"><i class="fab fa-twitter"></i><span class="sr-only">Twitter</span></a></li>
+        @endif
+        @if($insta && $insta != "")
+            <li><a href="{{$insta}}" rel="external" target="_blank"><i class="fab fa-instagram"></i><span class="sr-only">Instagram</span></a></li>
         @endif
     </ul>
-  @if (!have_posts())
-    No content to display yet.
-  @endif
 
-  @while (have_posts()) @php the_post() @endphp
-    @include('partials.content-'.get_post_type())
-  @endwhile
+    @if (!have_posts())
+        No content to display yet.
+    @endif
 
-  {!! get_the_posts_navigation() !!}
+    @while (have_posts()) @php the_post() @endphp
+        @include('partials.content-'.get_post_type())
+    @endwhile
+
+    {!! get_the_posts_navigation() !!}
 @endsection
