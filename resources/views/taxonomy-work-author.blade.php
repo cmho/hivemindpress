0e9116b0762187bea36a97b7129c959c276ad5aa
Carly Ho <cho@cliquestudios.com>
2020-01-13 16:13:05 -0600

    output more items

--- resources/views/taxonomy-work-author.blade.php
+++ resources/views/taxonomy-work-author.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
 @php
-    $author = pods_field('work-author', get_queried_object()->term_id, 'author_name');
-    print_r($author);
+    $author = pods_field('work-author', get_queried_object()->term_id, 'author_name', true);
+    $website = pods_field('work-author', get_queried_object()->term_id, 'website', true);
+    $itchio = pods_field('work-author', get_queried_object()->term_id, 'itchio', true);
 @endphp
-  <h2></h2>
+  <h2>{{ $author }}</h2>
   @if (!have_posts())
     No content to display yet.
   @endif
