2e215f163f5bca9b8912b2c6493ea2bc7f691807
Carly Ho <cho@cliquestudios.com>
2020-01-13 16:15:07 -0600

    start adding social stuff

--- resources/views/taxonomy-work-author.blade.php
+++ resources/views/taxonomy-work-author.blade.php
@@ -4,9 +4,20 @@
 @php
     $author = pods_field('work-author', get_queried_object()->term_id, 'author_name', true);
     $website = pods_field('work-author', get_queried_object()->term_id, 'website', true);
+    $bio = pods_field('work-author', get_queried_object()->term_id, 'bio', true);
     $itchio = pods_field('work-author', get_queried_object()->term_id, 'itchio', true);
+    $twitter = pods_field('work-author', get_queried_object()->term_id, 'twitter', true);
+    $insta = pods_field('work-author', get_queried_object()->term_id, 'instagram', true);
 @endphp
-  <h2>{{ $author }}</h2>
+  <h2>Work by {{ $author }}</h2>
+    <div class="bio">
+        {!! $bio !!}
+    </div>
+    <ul class="social">
+        @if($website && $website != "")
+            <li><a href="{{$website}}" rel="external" target="_blank"></a></li>
+        @endif
+    </ul>
   @if (!have_posts())
     No content to display yet.
   @endif
